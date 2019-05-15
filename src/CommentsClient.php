69866b80b90e3cf1326fb00881f41e8982806395
Alex B <alex.belenky@me.com>
2019-05-15 21:48:41 +0300

    Init

---
+++ src/CommentsClient.php
@@ -0,0 +1,50 @@
+<?php namespace PerspectiveApi;
+
+class CommentsClient {
+    public function __construct(string $token) {
+    }
+
+    public function analyze() {
+
+    }
+
+    public function suggestScore() {
+
+    }
+
+    public function comment() {
+
+    }
+
+    public function context() {
+
+    }
+
+    public function requestedAttributes() {
+
+    }
+
+    public function languages() {
+
+    }
+
+    public function doNotStore() {
+
+    }
+
+    public function clientToken() {
+
+    }
+
+    public function sessionId() {
+
+    }
+
+    public function attributeScores() {
+
+    }
+
+    public function communityId() {
+
+    }
+}
---
+++ src/CommentsResponse.php
@@ -0,0 +1,5 @@
+<?php namespace PerspectiveApi;
+
+class CommentsResponse {
+
+}
